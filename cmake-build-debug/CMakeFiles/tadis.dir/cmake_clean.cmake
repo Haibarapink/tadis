acc1574b1fda4179f48ccdb4173aabc59476b8fc
pink <haibarapink@gmail.com>
2023-01-02 19:30:56 +0800

    Init commit

---
+++ cmake-build-debug/CMakeFiles/tadis.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/tadis"
+  "../bin/tadis.pdb"
+  "CMakeFiles/tadis.dir/src/bin/main.cpp.o"
+  "CMakeFiles/tadis.dir/src/bin/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tadis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/bin/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <boost/asio.hpp>
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
