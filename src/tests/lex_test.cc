2ff5169a3cbbcbccbfa44960b110f17be0315b6b
pink <haibarapink@gmail.com>
2023-01-05 17:26:55 +0800

    添加词法解析的测试

--- src/tests/lex_test.cc
+++ src/tests/lex_test.cc
@@ -1,5 +1,50 @@
-#include <sql/lex.hpp>
 #include <boost/core/lightweight_test.hpp>
+#include <string>
+#include <vector>
+#include <sql/lex.hpp>
+
+void basic_lexer_unit_test()
+{
+  Lexer<std::string> lexer;
+  std::vector<std::string> words = {"hello", "-0.1", "world", "4321", "0.996", "000", "012"};
+  std::string input;
+  for (auto &i : words) {
+    input.append(i);
+    input.append(",");
+  }
+
+  auto commas_test = [&] {
+    auto [r, t, p] = lexer.internal_next();
+    BOOST_TEST(r == RC::SUCCESS);
+    BOOST_TEST(t == Token::COMMAS_T);
+    p++;
+  };
+
+  size_t p = 0;
+  {
+    auto [rc, token, last_p] = lexer.internal_next();
+    BOOST_TEST(rc == RC::SUCCESS);
+    BOOST_TEST(token == Token::ID_T);
+    BOOST_TEST(std::any_cast<std::string>(lexer.cur_val_ref()) == "hello");
+    commas_test();
+  }
+
+  {
+    auto [rc, token, last_p] = lexer.internal_next();
+    BOOST_TEST(rc == RC::SUCCESS);
+    BOOST_TEST(token == Token::FLOAT_T);
+    BOOST_TEST(std::any_cast<float>(lexer.cur_val_ref()) == -0.1);
+    commas_test();
+  }
+
+  {
+    auto [rc, token, last_p] = lexer.internal_next();
+    BOOST_TEST(rc == RC::SUCCESS);
+    BOOST_TEST(token == Token::ID_T);
+    BOOST_TEST(std::any_cast<std::string>(lexer.cur_val_ref()) == "world");
+    commas_test();
+  }
+}
 
 void test_simple_select()
 {
