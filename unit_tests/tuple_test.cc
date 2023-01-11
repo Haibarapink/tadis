030766ca44536bed19be649793aa35ff30214316
pink <haibarapink@gmail.com>
2023-01-11 23:00:06 +0800

    Fix bugs and add new unit tests

--- unit_tests/tuple_test.cc
+++ unit_tests/tuple_test.cc
@@ -39,8 +39,46 @@ void basic_test()
   BOOST_TEST(c.as_str() == "hello");
 }
 
+void basic_test2()
+{
+  long num = 100;
+  std::string name = "hello";
+  std::string name2 = "world";
+
+  Bytes bytes;
+  encode_varchar(bytes, name);
+  encode_num(bytes, num);
+  encode_char(bytes, name2);
+
+  TupleCellMeta m1 = TupleCellMeta::init("name", TupleCellType::VARCHAR);
+  TupleCellMeta m2 = TupleCellMeta::init("num", TupleCellType::INTEGER);
+  auto m3 = TupleCellMeta::init("name2", TupleCellType::CHAR, 5);
+  TupleMeta meta{m1, m2, m3};
+
+  Tuple tuple;
+  tuple.init(&meta, bytes);
+
+  TupleCell c;
+  BOOST_TEST(rc_success(tuple.get_cell(0, c)));
+  BOOST_TEST(c.as_str() == "hello");
+
+  BOOST_TEST(rc_success(tuple.get_cell(1, c)));
+  BOOST_TEST(c.as_integer() == 100);
+
+  BOOST_TEST(rc_success(tuple.get_cell(2, c)));
+  BOOST_TEST(c.as_str_view() == "world");
+
+  BOOST_TEST(rc_success(tuple.get_cell("num", c)));
+  BOOST_TEST(c.as_integer() == 100);
+  BOOST_TEST(rc_success(tuple.get_cell("name", c)));
+  BOOST_TEST(c.as_str() == "hello");
+  BOOST_TEST(rc_success(tuple.get_cell("name2", c)));
+  BOOST_TEST(c.as_str() == "world");
+}
+
 int main(int, char *[])
 {
   basic_test();
+  basic_test2();
   return boost::report_errors();
 }
